538c864b4a913d8b262941522fa9a908e3e6fb0e
guven <yguvenozcan@gmail.com>
2022-09-22 23:33:29 +0300

    musteri..

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+     class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla eklendi"+ musteri.Adi);
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri başarıyla silindi"+musteri.Adi);
+        }
+
+        public void Listeleme(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi : "+ musteri.Adi+ " "+ musteri.Soyadi +" " + musteri.Yas);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri();
+musteri1.Adi = "Güven";
+musteri1.Soyadi = "Özcan";
+musteri1.Yas = 23;
+
+Musteri musteri2 = new Musteri();
+musteri2.Adi = "X";
+musteri2.Soyadi = "Y";
+musteri2.Yas = 25;
+
+Musteri musteri3 = new Musteri();
+musteri3.Adi = "A";
+musteri3.Soyadi = "B";
+musteri3.Yas = 14;
+
+Musteri musteri4 = new Musteri();   
+musteri4.Adi = "C";
+musteri4.Soyadi = "D";
+musteri4.Yas = 10;
+
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.MusteriEkle(musteri1);
+musteriManager.MusteriEkle(musteri2);
+musteriManager.MusteriSil(musteri3);
+musteriManager.Listeleme(musteri4);
