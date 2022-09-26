f4dff22915b9000b4dde1d3431d7abf068666366
guven <yguvenozcan@gmail.com>
2022-09-26 18:17:40 +0300

    4

---
+++ OOP1/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP1;
+
+product product1 = new product();
+product1.Id = 1;
+product1.CategoryId = 2;
+product1.ProductName = "Masa";
+product1.UnitPrice = 500;
+product1.UnıtsInStock = 3;
+
+product product2 = new product { Id = 2, CategoryId = 5, UnıtsInStock = 5, ProductName = "Kalem", UnitPrice = 35 };
+
+ProductManager productManager = new ProductManager();
+productManager.Add(product1);
+
